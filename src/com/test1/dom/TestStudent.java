1bd63898b1c07ea526dec310880d2b91cf1e3397
bin <522392008@qq.com>
2018-04-03 22:11:04 +0800

    Commit Message

--- src/com/test1/dom/TestStudent.java
+++ src/com/test1/dom/TestStudent.java
@@ -1,7 +1,7 @@
 package com.test1.dom;
 
 public class TestStudent {
-    public void main(String[] args){
-        System.out.print("this is very beautiful");
+    public static void main(String[] args) {
+        System.out.println("hello world");
     }
 }
